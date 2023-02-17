f2bcd6301afe8ee7a8545541fe25a3f2713ad0a1
will <willhargrave@hotmail.co.uk>
2023-02-17 16:25:39 +0900

    diplay-style

--- src/components/DisplaySushi.js
+++ src/components/DisplaySushi.js
@@ -2,9 +2,9 @@ import React, {useState, useContext} from "react";
 import Sushi from "./Sushi";
 import ItemContext from "./ItemContext";
 import { useParams, Link} from "react-router-dom";
+import "../styles/Display.css"
 function DisplaySushi() {
     const [cartItems, setCartItems] = useContext(ItemContext)
-    console.log(Number(useParams))
     const { id } = useParams()
     const sushi = Sushi.find((p) => p.key === id);
  
@@ -15,11 +15,9 @@ function DisplaySushi() {
    
     }
     return (
-        <div>
-            
-            
+        <div className="sushi-card">
             <h1>{sushi.name}</h1>
-            <img src={sushi.img}></img>
+            <img src={sushi.img} className="sushi-img"></img>
             <Link to={'/'}>
               <button onClick={handleSubmit}>Add to Cart</button>
             </Link>
