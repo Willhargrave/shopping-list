ab9f8c529e1e6f320fafb52ad0f171f7ef8e910f
will <willhargrave@hotmail.co.uk>
2023-02-17 16:34:08 +0900

    card-styling

--- src/components/DisplaySushi.js
+++ src/components/DisplaySushi.js
@@ -15,12 +15,14 @@ function DisplaySushi() {
    
     }
     return (
+        <div className="sushi-container">
         <div className="sushi-card">
             <h1>{sushi.name}</h1>
             <img src={sushi.img} className="sushi-img"></img>
             <Link to={'/'}>
               <button onClick={handleSubmit}>Add to Cart</button>
             </Link>
+        </div>
         </div>
     )
 }
